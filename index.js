1fbea57fd5ae7188ec152174f515b585d5aaa6fc
Olha Melska <olya.melska@gmail.com>
2018-08-21 23:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const paragraph = document.querySelector('p');
 
 main.addEventListener('click', (event) => alert('Hey, you clicked me!'));
 
-paragraph.addEventListener('click', (event) => alert('You clicked a paragraph!'));
+paragraph.addEventListener('click', (event) => {
+  alert('You clicked a paragraph!')
+  paragraph.style.backgroundColor = 'yellow';
+  });
