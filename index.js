d31650669beadcd09dd6423f70b3958b73d99f59
Olha Melska <olya.melska@gmail.com>
2018-08-21 23:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const main = document.querySelector('#main');
 const paragraph = document.querySelector('p');
 
 main.addEventListener('click', (event) => alert('Hey, you clicked me!'));
+
+paragraph.addEventListener('click', (event) => alert('You clicked a paragraph!'));
